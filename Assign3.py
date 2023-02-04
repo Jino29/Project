844675706c521f223fd73ad98cdbf5a09a0c9880
Jino29 <124448909+Jino29@users.noreply.github.com>
2023-02-04 21:09:16 +0530

    Update Assign3.py

--- Assign3.py
+++ Assign3.py
@@ -1,14 +1,8 @@
 import random
-
-# List of fortune messages
 fortunes = [
     "Today will be a lucky day",
     "You will meet someone new and interesting",
     "Watch out for obstacles in your path"
 ]
-
-# Select a random fortune message from the list
 selected_fortune = random.choice(fortunes)
-
-# Display the selected fortune message
 print(selected_fortune)
